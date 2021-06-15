8328ef7b47ab0a408e44441a06bdcf5b13f5e080
taetae <peacepiece7@naver.com>
2021-06-15 21:50:31 +0900

    change from heroku cli -> github cli

--- source/init.js
+++ source/init.js
@@ -6,6 +6,9 @@ import "./models/User";
 import "./models/Comment";
 import app from "./server";
 
+// 포트를 아래와 같이 지정, heroku로 빌디 시 PORT를 random하게 뿌리고, localhost일 경우 4000으로 접속 함
+// const PORT = process.env.PORT || 4000
+
 function handleListen() {
   console.log(`🍎 connedted localhost ${process.env.PORT} PORT`);
 }
